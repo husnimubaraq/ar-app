3b0357becdb823a554697968eec2750fb6be2884
husni-ailatat <husniailatat@gmail.com>
2023-10-11 17:11:04 +0700

    feat: init ar (progress)

--- features/mode-ar/components/mode-ar-wrapper/index.tsx
+++ features/mode-ar/components/mode-ar-wrapper/index.tsx
@@ -1,10 +1,35 @@
-'use client'
+
 
 import Link from "next/link"
+import { useFrame, useLoader } from '@react-three/fiber';
+import {
+  ZapparCamera, ImageTracker, ZapparCanvas, Loader, BrowserCompatibility,
+} from '@zappar/zappar-react-three-fiber';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 import { HomeIcon, SoundIcon } from "components/icon"
+import { Suspense } from "react";
+import React from "react";
+
+let action: THREE.AnimationAction;
+
+const Model = () => {
+
+  const clock = new THREE.Clock();
+  const gltf = useLoader(GLTFLoader, "/waving.glb") as any ;
+  const mixer = new THREE.AnimationMixer(gltf.scene);
+
+  action = mixer.clipAction(gltf.animations[0]);
+  gltf.scene.rotateX(Math.PI / 2);
+
+  useFrame(() => mixer.update(clock.getDelta()));
+
+  return <primitive object={gltf.scene} />;
+};
 
 export const ModeArWrapper = () => {
+
   return (
     <div>
       <div className="flex justify-between items-end text-neutral-500">
@@ -17,7 +42,21 @@ export const ModeArWrapper = () => {
         <SoundIcon />
       </div>
 
-
+      <div>
+        <BrowserCompatibility />
+        <ZapparCanvas>
+          <ZapparCamera />
+          <Suspense fallback={null}>
+            <ImageTracker targetImage={"/example-tracking-image.zpt"}>
+              <React.Suspense fallback={null}>
+                <Model />
+              </React.Suspense>
+            </ImageTracker>
+          </Suspense>
+          <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
+          <Loader />
+        </ZapparCanvas>
+      </div>
     </div>
   )
 }
