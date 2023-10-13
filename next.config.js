7ba852623613f5cbefbc9a0310dc5d1d58aa8c61
Husni Ailatat <husniailatat@gmail.com>
2023-10-13 08:47:20 +0700

    init ar (progress)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
@@ -14,34 +15,12 @@ const nextConfig = {
     ],
   },
   webpack(config, { isServer }) {
-    if (!isServer) {
-      // We're in the browser build, so we can safely exclude the sharp module
-      config.externals.push('sharp')
-    }
-    // audio support
     config.module.rules.push({
-      test: /\.(ogg|mp3|wav|mpe?g)$/i,
-      exclude: config.exclude,
-      use: [
-        {
-          loader: require.resolve('url-loader'),
-          options: {
-            limit: config.inlineImageLimit,
-            fallback: require.resolve('file-loader'),
-            publicPath: `${config.assetPrefix}/_next/static/images/`,
-            outputPath: `${isServer ? '../' : ''}static/images/`,
-            name: '[name]-[hash].[ext]',
-            esModule: config.esModule || false,
-          },
-        },
-      ],
-    })
-
-    // shader support
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ['raw-loader', 'glslify-loader'],
+      test: /\.(zpt|glb)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/[hash][ext]',
+      },
     })
 
     return config
