72ce4ba50cd52c15e1da326913446dab6652c946
Husni Ailatat <husniailatat@gmail.com>
2023-10-15 17:20:12 +0700

    fix: remove url remote

--- features/mode-ar/components/mode-ar-wrapper/data.ts
+++ features/mode-ar/components/mode-ar-wrapper/data.ts
@@ -3,19 +3,19 @@ export const data = [
     {
         name: "raccoon",
         scale: "0.05 0.05 0.05",
-        // url: "./assets/models/raccoon/scene.gltf"
-        url: "https://raw.githubusercontent.com/husnimubaraq/ar-app/bc24ce6fddd78c5bb49d22b316c428614484d597/public/assets/models/raccoon/scene.gltf"
+        url: "./assets/models/raccoon/scene.gltf"
+        // url: "https://raw.githubusercontent.com/husnimubaraq/ar-app/bc24ce6fddd78c5bb49d22b316c428614484d597/public/assets/models/raccoon/scene.gltf"
     },
     {
         name: "bear",
         scale: "0.05 0.05 0.05",
-        // url: "./assets/models/bear/scene.gltf"
-        url: "https://raw.githubusercontent.com/husnimubaraq/ar-app/bc24ce6fddd78c5bb49d22b316c428614484d597/public/assets/models/bear/scene.gltf"
+        url: "./assets/models/bear/scene.gltf"
+        // url: "https://raw.githubusercontent.com/husnimubaraq/ar-app/bc24ce6fddd78c5bb49d22b316c428614484d597/public/assets/models/bear/scene.gltf"
     },
     {
         name: "fox",
         scale: "0.01 0.01 0.01",
-        // url: "./assets/models/fox/Fox.gltf"
-        url: "https://raw.githubusercontent.com/husnimubaraq/ar-app/bc24ce6fddd78c5bb49d22b316c428614484d597/public/assets/models/fox/Fox.gltf"
+        url: "./assets/models/fox/Fox.gltf"
+        // url: "https://raw.githubusercontent.com/husnimubaraq/ar-app/bc24ce6fddd78c5bb49d22b316c428614484d597/public/assets/models/fox/Fox.gltf"
     },
 ]
