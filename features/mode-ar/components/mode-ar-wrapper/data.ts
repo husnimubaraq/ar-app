a3cad6f2fd1d2e64287be5e2f1aa2650c2d84e3f
Husni Ailatat <husniailatat@gmail.com>
2023-10-15 16:15:47 +0700

    feat: finishing mind ar

---
+++ features/mode-ar/components/mode-ar-wrapper/data.ts
@@ -0,0 +1,27 @@
+
+export const data = [
+    {
+        name: "raccoon",
+        scale: "0.05 0.05 0.05",
+        url: "./assets/models/raccoon/scene.gltf"
+        // url: "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.2/examples/image-tracking/assets/band-example/raccoon/scene.gltf"
+    },
+    {
+        name: "bear",
+        scale: "0.05 0.05 0.05",
+        url: "./assets/models/bear/scene.gltf"
+        // url: "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.2/examples/image-tracking/assets/band-example/bear/scene.gltf"
+    },
+    {
+        name: "fox",
+        scale: "0.01 0.01 0.01",
+        url: "./assets/models/fox/Fox.gltf"
+        // url: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/master/2.0/Fox/glTF/Fox.gltf"
+    },
+    {
+        name: "avocado",
+        scale: "0.01 0.01 0.01",
+        url: "./assets/models/avocado/Avocado.gltf"
+        // url: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/master/2.0/Fox/glTF/Fox.gltf"
+    }
+]
--- features/mode-ar/components/mode-ar-wrapper/index.tsx
+++ features/mode-ar/components/mode-ar-wrapper/index.tsx
@@ -1,28 +1,46 @@
 // @ts-nocheck
 'use client'
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import { HomeIcon, SoundIcon } from 'components/icon';
+
+import { data } from './data'
 
 import 'aframe';
 import 'mind-ar/dist/mindar-image-aframe.prod.js';
 
 export const ModeArWrapper = () => {
 
-  const sceneRef = useRef<any>(null);
+  const sceneRef = useRef<any>(null)
+  
+  const [name, setName] = useState('')
 
   useEffect(() => {
-    const sceneEl = sceneRef?.current ?? null;
+    const sceneEl = sceneRef?.current ?? null
     if (sceneEl) {
-      const arSystem = sceneEl?.systems["mindar-image-system"] ?? null;
+      const arSystem = sceneEl?.systems["mindar-image-system"] ?? null
       sceneEl?.addEventListener('renderstart', () => {
         if (arSystem) {
-          arSystem?.start(); // start AR 
+          arSystem?.start()
         }
-      });
+      })
     }
-  }, []);
+  }, [])
+
+  useEffect(() => {
+    for(let item of data){
+      const target = document.querySelector(`#${item.name}-target`)
+
+      target?.addEventListener('targetFound', () => {
+        setName(item.name.toUpperCase())
+      })
+
+      target?.addEventListener('targetLost', () => {
+        setName('')
+      })
+    }
+  }, [])
 
 
   return (
@@ -38,42 +56,31 @@ export const ModeArWrapper = () => {
       </div>
 
       <div className="container">
-        <a-scene ref={sceneRef} mindar-image="imageTargetSrc: public/targets.mind" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+        <a-scene ref={sceneRef} mindar-image="imageTargetSrc: ./targets.mind;uiScanning: no;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
           <a-assets>
-            <a-asset-item id="bearModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.2/examples/image-tracking/assets/band-example/bear/scene.gltf"></a-asset-item>
-            <a-asset-item id="raccoonModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.2/examples/image-tracking/assets/band-example/raccoon/scene.gltf"></a-asset-item>
-            <a-asset-item id="foxModel" src="https://github.com/KhronosGroup/glTF-Sample-Models/blob/master/2.0/Fox/glTF/Fox.gltf"></a-asset-item>
+            {data.reverse().map((item) => (
+              <a-asset-item 
+                id={`${item.name}-model`} src={item.url}
+              ></a-asset-item>
+            ))}
           </a-assets>
 
           <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
 
-          <a-entity mindar-image-target="targetIndex: 0">
-            <a-entity 
-              position="0 0.50 0" 
-              geometry="primitive: plane; width: 0.3; height: auto" 
-              material="color: orange"
-              text="color: white; align: center; value: Raccoon; width: 1; "></a-entity>
-            <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#raccoonModel" animation-mixer></a-gltf-model>
-          </a-entity>
-
-          <a-entity mindar-image-target="targetIndex: 1">
-            <a-entity 
-              position="0 0.50 0" 
-              geometry="primitive: plane; width: 0.3; height: auto" 
-              material="color: orange"
-              text="color: white; align: center; value: Bear; width: 1; "></a-entity>
-            <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#bearModel" animation-mixer></a-gltf-model>
-          </a-entity>
-
-          <a-entity mindar-image-target="targetIndex: 2">
-            <a-entity 
-              position="0 0.50 0" 
-              geometry="primitive: plane; width: 0.3; height: auto" 
-              material="color: orange"
-              text="color: white; align: center; value: Bear; width: 1; "></a-entity>
-            <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#foxModel" animation-mixer></a-gltf-model>
-          </a-entity>
+          {data.map((item, index) => (
+            <a-entity id={`${item.name}-target`} mindar-image-target={`targetIndex: ${index}`}>
+              <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale={item.scale} src={`#${item.name}-model`} animation-mixer></a-gltf-model>
+            </a-entity>
+          ))}
         </a-scene>
+
+        {name && (
+          <div className='fixed top-[11%] left-10 right-10'>
+            <div className='bg-white rounded-md py-2 px-5'>
+              <h4 className='text-xl font-bold text-black text-center'>{name}</h4>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
