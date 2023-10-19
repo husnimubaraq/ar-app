e965c3ce14313aa085500d05201af213f7a36909
Husni Ailatat <husniailatat@gmail.com>
2023-10-19 14:27:42 +0700

    fix: fix model ar

--- features/mode-ar/components/mode-ar-wrapper/data.ts
+++ features/mode-ar/components/mode-ar-wrapper/data.ts
@@ -1,21 +1,165 @@
 
 export const data = [
     {
-        name: "raccoon",
+        name: "cardboard_box",
+        title: "Kontak Kardus",
         scale: "0.05 0.05 0.05",
-        url: "./assets/models/raccoon/scene.gltf"
-        // url: "https://raw.githubusercontent.com/husnimubaraq/ar-app/bc24ce6fddd78c5bb49d22b316c428614484d597/public/assets/models/raccoon/scene.gltf"
+        url: "./assets/models/cardboard_box/scene.gltf"
     },
     {
-        name: "bear",
-        scale: "0.05 0.05 0.05",
-        url: "./assets/models/bear/scene.gltf"
-        // url: "https://raw.githubusercontent.com/husnimubaraq/ar-app/bc24ce6fddd78c5bb49d22b316c428614484d597/public/assets/models/bear/scene.gltf"
+        name: "clock",
+        title: "Jam Dinding",
+        scale: "3 3 3",
+        url: "./assets/models/clock/scene.gltf"
     },
     {
-        name: "fox",
-        scale: "0.01 0.01 0.01",
-        url: "./assets/models/fox/Fox.gltf"
-        // url: "https://raw.githubusercontent.com/husnimubaraq/ar-app/bc24ce6fddd78c5bb49d22b316c428614484d597/public/assets/models/fox/Fox.gltf"
+        name: "book",
+        title: "Buku",
+        scale: "0.1 0.1 0.1",
+        url: "./assets/models/book/scene.gltf"
+    },
+    {
+        name: "kite",
+        title: "Layangan",
+        scale: "0.5 0.5 0.5",
+        url: "./assets/models/kite/scene.gltf"
+    },
+    {
+        name: "cardboard_lowpoly",
+        title: "Kotak Snack",
+        scale: "3 3 3",
+        url: "./assets/models/cardboard_lowpoly/scene.gltf"
+    },
+    {
+        name: "soccer-ball",
+        title: "Bola",
+        scale: "1 1 1",
+        url: "./assets/models/soccer-ball/Fox.gltf"
+    },
+    {
+        name: "whiteboard",
+        title: "Papan Tulis",
+        scale: "0.5 0.5 0.5",
+        url: "./assets/models/whiteboard/scene.gltf"
+    },
+    {
+        name: "marbles",
+        title: "Kelereng",
+        scale: "15 15 15",
+        url: "./assets/models/marbles/scene.gltf"
+    },
+    {
+        name: "smartphone",
+        title: "HP",
+        scale: "0.5 0.5 0.5",
+        url: "./assets/models/smartphone/scene.gltf"
+    },
+    {
+        name: "bucket",
+        title: "Ember",
+        scale: "0.05 0.05 0.05",
+        url: "./assets/models/bucket/scene.gltf"
+    },
+    {
+        name: "wheel",
+        title: "Roda",
+        scale: "0.4 0.4 0.4",
+        url: "./assets/models/wheel/scene.gltf"
+    },
+    {
+        name: "dice",
+        title: "Dadu",
+        scale: "1 1 1",
+        url: "./assets/models/dice/scene.gltf"
+    },
+    {
+        name: "balloons",
+        title: "Balon",
+        scale: "3 3 3",
+        url: "./assets/models/balloons/scene.gltf"
+    },
+    {
+        name: "can",
+        title: "Kaleng",
+        scale: "0.1 0.1 0.1",
+        url: "./assets/models/can/scene.gltf"
+    },
+    {
+        name: "plates",
+        title: "Piring",
+        scale: "0.02 0.02 0.02",
+        url: "./assets/models/plates/scene.gltf"
+    },
+    {
+        name: "rubirc",
+        title: "Rubrik",
+        scale: "15 15 15",
+        url: "./assets/models/rubirc/scene.gltf"
+    },
+    {
+        name: "eraser",
+        title: "Penghapus Papan",
+        scale: "0.05 0.05 0.05",
+        url: "./assets/models/eraser/scene.gltf"
+    },
+    {
+        name: "radio",
+        title: "Radio",
+        scale: "0.005 0.005 0.005",
+        url: "./assets/models/radio/scene.gltf"
+    },
+    {
+        name: "cup",
+        title: "Cangkir",
+        scale: "0.5 0.5 0.5",
+        url: "./assets/models/cup/scene.gltf"
+    },
+    {
+        name: "hanger",
+        title: "Hanger",
+        scale: "0.05 0.05 0.05",
+        url: "./assets/models/hanger/scene.gltf"
+    },
+    {
+        name: "chair",
+        title: "Kursi",
+        scale: "0.5 0.5 0.5",
+        url: "./assets/models/chair/scene.gltf"
+    },
+    {
+        name: "bell",
+        title: "Lonceng",
+        scale: "0.003 0.003 0.003",
+        url: "./assets/models/bell/scene.gltf"
+    },
+    {
+        name: "fan",
+        title: "Kipas Angin",
+        scale: "0.0005 0.0005 0.0005",
+        url: "./assets/models/fan/scene.gltf"
+    },
+    {
+        name: "bottle",
+        title: "Botol Minum",
+        scale: "0.15 0.15 0.15",
+        url: "./assets/models/bottle/scene.gltf"
+    },
+    {
+        name: "glass_bowl",
+        title: "Mangkuk",
+        scale: "15 15 15",
+        url: "./assets/models/glass_bowl/scene.gltf"
+    },
+    {
+        name: "conical_hat",
+        title: "Caping",
+        scale: "5 5 5",
+        url: "./assets/models/conical_hat/scene.gltf"
+    },
+    {
+        name: "gas",
+        title: "Tabung Gas",
+        scale: "1 1 1",
+        url: "./assets/models/gas/scene.gltf"
     },
 ]
