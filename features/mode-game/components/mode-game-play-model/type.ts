aa26c81637224e4553a6bb7cdb85caaa1cca82af
Husni Ailatat <husniailatat@gmail.com>
2023-10-19 13:54:18 +0700

    feat: integrate mode-game (progress)

--- features/mode-game/components/mode-game-play-model/type.ts
+++ features/mode-game/components/mode-game-play-model/type.ts
@@ -1,3 +1,4 @@
 export type TProps = {
     glb: any
+    scale: number
 }
--- features/mode-game/components/mode-game-wrapper/index.tsx
+++ features/mode-game/components/mode-game-wrapper/index.tsx
@@ -9,16 +9,28 @@ import { twMerge } from "tailwind-merge"
 import { useState } from "react"
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
+import Image from "next/image"
+import { SuccessPopup } from "components/success-popup"
+import { FailedPopup } from "components/failed-popup"
 
 export const ModeGameWrapper = () => {
 
+  const [isOpen, setIsOpen] = useState(false)
+  const [type, setType] = useState('')
+
   const [tabIndex, setTabIndex] = useState(0)
   const [gameIndex, setGameIndex] = useState(0)
-
   const data = dataGame.find((item) => item.level === dataGame[tabIndex].level)
 
   const onNext = () => {
-    setGameIndex(gameIndex + 1)
+    if (data) {
+      if (data.game.length - 1 === gameIndex) {
+        setTabIndex(tabIndex + 1)
+        setGameIndex(0)
+      } else {
+        setGameIndex(gameIndex + 1)
+      }
+    }
   }
 
   return (
@@ -36,19 +48,25 @@ export const ModeGameWrapper = () => {
       <div className="w-full max-w-md px-2 mt-5 sm:px-0">
         <Tab.Group
           onChange={(index) => {
-            setTabIndex(index)
-            setGameIndex(0)
+            if (tabIndex < index) {
+
+            } else {
+              setTabIndex(index)
+              setGameIndex(0)
+            }
           }}
         >
           <Tab.List className="flex space-x-1 rounded-xl bg-orange-900/20 p-1">
-            {dataGame.map((item) => (
+            {dataGame.map((item, index) => (
               <Tab
                 key={item.level}
+                disabled={tabIndex < index ? true : false}
                 className={({ selected }) =>
                   twMerge(
                     'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-orange-700',
                     'ring-white ring-opacity-60 ring-offset-2 ring-offset-orange-400 focus:outline-none focus:ring-2',
-                    selected
+                    tabIndex < index && 'bg-neutral-300',
+                    tabIndex === index
                       ? 'bg-white shadow'
                       : 'text-orange-500 hover:bg-white/[0.12] hover:text-white'
                   )
@@ -65,24 +83,36 @@ export const ModeGameWrapper = () => {
                 {data && (
                   <ModeGamePlayModel
                     glb={data.game[gameIndex].model}
+                    scale={data.game[gameIndex].scale}
                   />
                 )}
               </group>
 
               <OrbitControls />
 
-              <Environment 
-                preset="apartment" 
+              <Environment
+                preset="apartment"
                 background
                 blur={0.65}
               />
             </Canvas>
-            <div 
-              onClick={onNext}
-              className="bg-neutral-200 rounded-md py-2 w-[50%] hover:bg-orange-500 hover:text-white text-sm text-center cursor-pointer absolute bottom-[5%] left-0 right-0 mx-auto"
-            >
-              {data && data.game.length - 1 === gameIndex ? 'Selesai' : 'Lanjut'}
-            </div>
+            {data && data.game.length - 1 === gameIndex ? (
+              <div
+                onClick={dataGame.length - 1 === tabIndex ? undefined : onNext}
+                className="bg-neutral-200 rounded-md py-2 w-[50%] hover:bg-orange-500 hover:text-white text-sm text-center cursor-pointer absolute bottom-[5%] left-0 right-0 mx-auto"
+              >
+                Selesai
+              </div>
+            ) : (
+              <div
+                onClick={onNext}
+                className="bg-neutral-200 rounded-md py-2 w-[50%] hover:bg-orange-500 hover:text-white text-sm text-center cursor-pointer absolute bottom-[5%] left-0 right-0 mx-auto"
+              >
+                {data && data.game.length - 1 === gameIndex ? 'Selesai' : 'Lanjut'}
+              </div>
+            )}
+
+            <FailedPopup/>
           </div>
         </Tab.Group>
       </div>
